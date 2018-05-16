7d17c6b5f34309b2e825953fd37ed91bb3546e94
saidurrahmanshawon <37071374+saidurrahmanshawon@users.noreply.github.com>
2018-05-16 18:40:08 +0600

    first upload

---
+++ index (3).php
@@ -0,0 +1,130 @@
+<html>
+    <head>
+        <meta name="google-site-verification" content="6EgfXYY4SpBhHMD01ZljONuZZ5VqKSqTShuDn-q-X30" />    
+	    <meta name="propeller" content="79b67de8e570fd9977e79edc1b67eac0" />
+	    <title>LIVE TV</title>
+		<link href="newcss.css" rel="stylesheet" type="text/css">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+	</head>	
+    <body>
+    <div style="float:right">
+     <!-- Begin BidVertiser code -->
+<SCRIPT data-cfasync="false" SRC="//bdv.bidvertiser.com/BidVertiser.dbm?pid=798888&bid=1927799" TYPE="text/javascript"></SCRIPT>
+<!-- End BidVertiser code --> </div>
+    <!-- Begin BidVertiser code -->
+<SCRIPT data-cfasync="false" SRC="//bdv.bidvertiser.com/BidVertiser.dbm?pid=798888&bid=1927537" TYPE="text/javascript"></SCRIPT>
+<!-- End BidVertiser code -->
+     <!-- Begin BidVertiser code -->
+<SCRIPT data-cfasync="false" SRC="//bdv.bidvertiser.com/BidVertiser.dbm?pid=798888&bid=1927537" TYPE="text/javascript"></SCRIPT>
+<!-- End BidVertiser code --> 
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- ad1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2408629149137962"
+     data-ad-slot="8724414823"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+	<script type="text/javascript" src="//go.oclasrv.com/apu.php?zoneid=1491832"></script>
+     
+	<h2 style=color:orange;>Date:<?php
+	echo date("d/m/y") . "<br>"
+	?>
+    Time now:<?php
+        //converting to a string from timestamp
+        date_default_timezone_set('Asia/Dhaka');
+        print date("h:i:s A");
+    ?>    
+    </h2>
+	<div class="page-header";float:center;>
+      <h1 style=color:red;><b>WATCH LIVE TV ONLINE</b></h1>
+      </div>
+    
+    <nav>
+        <div class="toggle">
+    	    <i class="fa fa-bars menu" aria-hidden="true"></i>
+		</div>
+        <ul>
+            <li><a href="http://www.shawon.rf.gd"><b>Home</b></a></li>
+			<li><a href="form1/form1.php"><b>form</b></a></li>
+            <li><a href="app.php"><b>EXTREM App</b></a></li>
+			<li><a href="#">Bangla channal</a>
+			    <ul>
+				    <li><a href="https://www.youtube.com/watch?v=DPYDaT7UMNw">SOMOY TV live</a></li>
+         		 	<li><a href="https://www.youtube.com/watch?v=EqJLKC7s3Ds">Rtv Live</a></li>
+					<li><a href="https://www.youtube.com/watch?v=fyoo6TvwqG4">GTV Live</a></li>
+					<li><a href="https://www.youtube.com/watch?v=IgyZQ-bEiHA">NTV live</a></li>
+					<li><a href="https://www.youtube.com/watch?v=c8zM70EasRg">NEWS24 Live</a></li>
+					<li><a href="https://www.youtube.com/watch?v=t3WLuBnL08I">ATN News Live</a></li>
+				</ul>
+			</li>
+			<li><a>sports channal</a>
+			    <ul>
+			        <li><a href="http://www.imgslider.com/tv.php">skyspots</a></li>
+			        <li><a href="http://185.21.217.54:8554/load/hero2018/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9My82LzIwMTggNDowNzo1MCBQTSZoYXNoX3ZhbHVlPWtvNWQyMmc1WXhtV0VBd296bC9oTWc9PSZ2YWxpZG1pbnV0ZXM9Mg==">live match</a></li>
+			        <li><a href="http://185.21.217.29:9100/ape/discoverybangla/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9My83LzIwMTggMTA6NDk6MDEgQU0maGFzaF92YWx1ZT1WeHJUMjh5ZUJoTllFNUp4aEovQlhRPT0mdmFsaWRtaW51dGVzPTI=">Discovery bangla</a></li>
+			        <li><a href="http://185.21.216.188:8554/gorilla/historytv18in/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9My83LzIwMTggMTA6NTg6MjUgQU0maGFzaF92YWx1ZT1lZjAxTnIzOVJUWXlZNXZYNkpiTk9RPT0mdmFsaWRtaW51dGVzPTI=">History hindi</a></li>
+			        <li><a href="http://185.21.216.193:8554/live/starsp1/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9My83LzIwMTggMTE6MDc6MDcgQU0maGFzaF92YWx1ZT1sdFBWZ09TZ2JxNGVFUXh1VEJkN05nPT0mdmFsaWRtaW51dGVzPTI=">star spors 1</a></li>
+			        <li><a href="#">services 6</a></li>
+				</ul>	
+		    </li>
+			<li>
+                <ul><li><a href="#">comming soon</a></li>
+			</li>
+            <li><a href="#">Hinde channal</a></li>
+			<li><a href="#">cartoon channal</a>
+			     <ul>
+				     <li><a href="">sponsbob</a>
+				</ul>
+			</li>	
+			<li><a href="#">news channal</a>
+			    <ul>
+			        <li><a href="https://www.youtube.com/watch?v=1u7JPqjpoi8">Fox News Live</a>
+					</li>
+                </ul>    
+			</li>	
+			<li><a href="#">Other channal</a>
+			    <ul>
+				     <li><a href="https://www.youtube.com/watch?v=RtU_mdL2vBM">NASA Live</a>
+					 </li>   
+			    </ul>
+            </li>    
+            <li><a href="https://www.facebook.com/md.shawon.3323">website owner</li>
+		</ul>
+	</nav>
+    <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+    	<script type="text/javascript">
+		    $(document).ready(function(){
+			    $('.menu').click(function(){
+				    $('ul').toggleClass('active');
+				})
+			})
+		</script>
+    <br /><br /><br /><br /><br />
+     <!-- Begin BidVertiser code -->
+<SCRIPT data-cfasync="false" SRC="//bdv.bidvertiser.com/BidVertiser.dbm?pid=798888&bid=1927537" TYPE="text/javascript"></SCRIPT>
+<!-- End BidVertiser code -->  <!-- Begin BidVertiser code -->
+<SCRIPT data-cfasync="false" SRC="//bdv.bidvertiser.com/BidVertiser.dbm?pid=798888&bid=1927537" TYPE="text/javascript"></SCRIPT>
+<!-- End BidVertiser code --> 
+	<P><B><a href="https://go.pub2srv.com/afu.php?id=1491816">click hare</a></b></p>ad
+	<br /><br /><br /><br />
+	<h1><a href="https://go.pub2srv.com/afu.php?id=1491816">Rtv live</h1><br />
+    <iframe width="420" height="300" src="https://www.youtube.com/embed/fyoo6TvwqG4?ecver=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    <div style="float:right"><h1>live match</h1>
+    <iframe width="520" height="400" src="http://159.65.151.181:8081/HDSTMIPL5y547hth/ben_360/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9NC8zMC8yMDE4IDU6MzI6MzkgUE0maGFzaF92YWx1ZT1sZFF6NDM0WGtlRzN6RlB2SU5UK1J3PT0mdmFsaWRtaW51dGVzPTU=" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    </div>
+    <iframe width="420" height="300" src="http://185.21.216.136:8554/eleven/abc/dsports/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9My8xMC8yMDE4IDY6MTg6NDkgQU0maGFzaF92YWx1ZT1jZnZpd2xHMWp5WDJxMFJzeUdXQlRnPT0mdmFsaWRtaW51dGVzPTI=" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    
+    <!-- Begin BidVertiser code -->
+<SCRIPT data-cfasync="false" SRC="//bdv.bidvertiser.com/BidVertiser.dbm?pid=798888&bid=1927537" TYPE="text/javascript"></SCRIPT>
+<!-- End BidVertiser code --> 
+     <!-- Begin BidVertiser code -->
+<SCRIPT data-cfasync="false" SRC="//bdv.bidvertiser.com/BidVertiser.dbm?pid=798888&bid=1927537" TYPE="text/javascript"></SCRIPT>
+<!-- End BidVertiser code --> 
+    <!-- Begin BidVertiser code -->
+<SCRIPT data-cfasync="false" SRC="//bdv.bidvertiser.com/BidVertiser.dbm?pid=798888&bid=1927537" TYPE="text/javascript"></SCRIPT>
+<!-- End BidVertiser code --> 
+    </body>
+</html>
